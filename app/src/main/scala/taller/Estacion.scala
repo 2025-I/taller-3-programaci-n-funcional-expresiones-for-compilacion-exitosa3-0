1dd26c43b60e110f0e6a6667ebfd6a33ed18f31a
diego gomez <diego.puentes@correounivalle.edu.co>
2025-04-28 18:22:33 -0500

    enviando funcion aplicarMovimiento

---
+++ app/src/main/scala/taller/Estacion.scala
@@ -0,0 +1,44 @@
+package taller
+
+object ManiobrasTrenes {
+
+  type Vagon = Any
+  type Tren = List[Vagon]
+  type Estado = (Tren, Tren, Tren)
+
+  trait Movimiento
+  case class Uno(n: Int) extends Movimiento
+  case class Dos(n: Int) extends Movimiento
+
+  class Estacion {
+    def aplicarMovimiento(e: Estado, m: Movimiento): Estado = {
+      val (principal, uno, dos) = e
+
+      (for {
+        resultado <- m match {
+          case Uno(n) if n > 0 =>
+            val (quedan, seVan) = principal.splitAt(principal.length - (n min principal.length))
+            Some((quedan, uno ++ seVan, dos))
+
+          case Uno(n) if n < 0 =>
+            val (vuelven, quedan) = uno.splitAt((-n) min uno.length)
+            Some((principal ++ vuelven, quedan, dos))
+
+          case Dos(n) if n > 0 =>
+            val (quedan, seVan) = principal.splitAt(principal.length - (n min principal.length))
+            Some((quedan, uno, dos ++ seVan))
+
+          case Dos(n) if n < 0 =>
+            val (vuelven, quedan) = dos.splitAt((-n) min dos.length)
+            Some((principal ++ vuelven, uno, quedan))
+
+          case _ =>
+            Some(e)
+        }
+      } yield resultado) match {
+        case Some(estado) => estado
+        case None => e
+      }
+    }
+  }
+}
